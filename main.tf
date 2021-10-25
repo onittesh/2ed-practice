e380a391fdb3eaf2ef194a157d778674ac37f6fc
nittesh <o.nittesh@gmail.com>
2021-10-25 13:31:31 +0530

    added name

--- main.tf
+++ main.tf
@@ -2,7 +2,4 @@
 echo nittesh
 read -p Enter The Name With capital letteers 
 read -p Enter The Name Withn Small letteers
-if small letters start with a print small 
-if capital letter start with a print capital 
-then
-go to the 
+#nittesh
