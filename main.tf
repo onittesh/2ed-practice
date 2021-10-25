858d8181d9663c69062e2ffb8624d83eba4b8921
nittesh <o.nittesh@gmail.com>
2021-10-25 09:43:40 +0530

    small letters entered

--- main.tf
+++ main.tf
@@ -1,2 +1,6 @@
 # hellop world
 echo nittesh
+read -p Enter The Name With capital letteers 
+read -p Enter The Name Withn Small letteers
+if small letters start with a print small 
+if capital letter start with a print capital 
