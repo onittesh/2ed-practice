357c45ae76294682e3e587a4cab4fd83b0ecbd8f
nittesh <o.nittesh@gmail.com>
2021-10-25 09:58:28 +0530

    entered the extra leters

--- main.tf
+++ main.tf
@@ -4,3 +4,5 @@ read -p Enter The Name With capital letteers
 read -p Enter The Name Withn Small letteers
 if small letters start with a print small 
 if capital letter start with a print capital 
+then
+go to the 
