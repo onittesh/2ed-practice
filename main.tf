08ca1b81486ab96586e6dcca36d85f6b09d3a4e0
nittesh <o.nittesh@gmail.com>
2021-10-26 11:57:16 +0530

    acd ABCD

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ echo nittesh
 read -p Enter The Name With capital letteers 
 read -p Enter The Name Withn Small letteers
 #nittesh
- A B C D 
+ A B C D biosdike
+ 
--- shell.sh
+++ shell.sh
@@ -1,1 +1,2 @@
 shell.sh is linux word
+abcdefghi
